6d924702f86d81af8140d86d60bb75f7a2790a58
patrizineversuxxx <r2tr@yandex.ru>
2018-05-30 18:13:02 +0400

    Added hero and hero movement.

--- src/Game/Drawing/GameWindow.java
+++ src/Game/Drawing/GameWindow.java
@@ -7,10 +7,12 @@ public class GameWindow extends JFrame {
     private GameField gameField;
 
     public GameWindow(){
+        //Adding game field on main game window
         gameField = new GameField();
         Container container = getContentPane();
         container.add(gameField);
 
+        //Setting game window properties
         setBounds(0,0,800,600);
         setTitle("GiftsSnow: THE GAME");
         setVisible(true);
